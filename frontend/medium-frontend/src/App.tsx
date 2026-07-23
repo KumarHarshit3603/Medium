f7c35b96fb72ac15900a1e89b514f14bb65457f3
Harshit Kumar <kumarharshit3603@gmail.com>
2026-07-23 16:43:44 +0530

    added editor

--- frontend/medium-frontend/src/App.tsx
+++ frontend/medium-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Signuppage from './routes/Signuppage'
 import Signinpage from './routes/Signinpage'
 import Home from './routes/Homepage'
 import Blogpage from './routes/blogpage'
+import Newblog from './routes/newblogpage'
 export default function App(){
   return (
     <>
@@ -12,6 +13,7 @@ export default function App(){
           <Route path = "/Signin" element = {<Signinpage></Signinpage>}></Route>
           <Route path = "/" element = {<Home/>}/>
           <Route path = "/blogpage" element = {<Blogpage></Blogpage>}/>
+          <Route path = "/newblog" element = {<Newblog></Newblog>}/>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/medium-frontend/src/components/editor.tsx
+++ frontend/medium-frontend/src/components/editor.tsx
@@ -1,14 +1,44 @@
 import EditorJS from '@editorjs/editorjs'
+import Header from '@editorjs/header'
+import Quote from '@editorjs/quote'
+import List from '@editorjs/list'
+import NestedList from '@editorjs/nested-list'
+import Checklist from '@editorjs/checklist'
+import Image from '@editorjs/image'
+import SimpleImage from '@editorjs/simple-image'
+import Link from '@editorjs/link'
+import Table from '@editorjs/table'
+import Code from '@editorjs/code'
 
+import {useState, useEffect} from 'react'
 
-const editor = new EditorJS({
-    holder: 'editor'
-})
 
 export default function Editor(){
-    return(
-        <div id = "editor">
+        
+    useEffect(()=>{
+            const editor = new EditorJS({
+            holder: 'editor',
+            tools:{
+                header: Header,
+                quote:Quote,
+                list:List,
+                // nestedlist:NestedList,
+                // image:Image,
+                // simpleimage:SimpleImage,
+                link:Link,
+                table:Table,
+                code:Code,
 
+            }
+        })
+
+        // return(()=>{
+        //     editor.destroy();
+        // });
+    },[])
+    return(
+        <div className = "prose max-w-none">
+            <div id = "editor"></div>
         </div>
     )
 }
